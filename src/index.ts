cce65cdda078c419717d77c6f6bddb692502bc4e
PierreTDX <pierre.tondeux@gmail.com>
2025-10-14 13:45:45 +0200

    initialisation du server

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server is working!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
